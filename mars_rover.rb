0a6103bb209d5c6ff1d11df20db7a5cc3ba94007
Michael Atlin <mike.atlin@gmail.com>
2014-01-16 09:34:21 -0500

    Rover logic complete

--- mars_rover.rb
+++ mars_rover.rb
@@ -4,8 +4,8 @@ def turn(order, old_heading)
 	when "E" then num_heading = 1
 	when "S" then num_heading = 2
 	when "W" then num_heading = 3
-	else p "Heading error"
-		return
+	else puts "Heading error.  Old heading retained"
+		return old_heading
 	end
 
 	if order == "R"
@@ -20,31 +20,56 @@ def turn(order, old_heading)
 	when 1 then new_heading = "E"
 	when 2 then new_heading = "S"
 	when 3 , -1 then new_heading = "W"
-	else p "Heading error"
-		return
+	else puts "Heading error.  Old heading retained"
+		return old_heading
 	end
+	return new_heading
 end
 
-def move(xy,heading)
-	case heading
-	when "N" then xy[0] += 1
-	when "E" then xy[1] += 1
-	when "S" then xy[0] -= 1
-	when "W" then xy[1] -= 1
+def move(position, bounds)
+	new_position = position
+	case new_position[2]
+	when "N" then position[1] += 1
+	when "E" then position[0] += 1
+	when "S" then position[1] -= 1
+	when "W" then position[0] -= 1
 	else puts "Heading error"
 	end
-end
-
-
-#test
-directions = ["N","E","S","W"]
-orders = ["R", "L"]
-xy = [0,0]
-directions.each do |cardinel|
-	orders.each do |l_s|
-		heading = turn(l_s, cardinel)
-		puts heading
-		move(xy,heading)
-		puts xy
+	if new_position[0] > bounds[0] || new_position[1] > bounds[1] || new_position[0] < 0 || new_position[1] < 0
+		puts "Danger!  Rover headed out of bounds.  Move aborted.  Continuing with next instruction."
+		return position
+	else
+		return new_position
 	end
 end
+
+
+def execute_orders(position,orders, bounds)
+	orders.each do |this_order|
+		if this_order == "M"
+			position = move(position, bounds)
+		elsif this_order == "L"|| "R"
+			position[2] = turn(this_order,position[2])
+		end
+		print position
+		print "\n"
+	end
+end
+
+bounds = [5, 5]
+position = [1, 2, "N"]
+orders = "LMLMLMLMM".split("")
+
+execute_orders(position, orders, bounds)
+#test1
+# directions = ["N","E","S","W"]
+# orders = ["R", "L"]
+# xy = [0,0]
+# directions.each do |cardinel|
+# 	orders.each do |l_s|
+# 	|	heading = turn(l_s, cardinel)
+# 		puts heading
+# 		move(xy,heading)
+# 		puts xy
+# 	end
+# end
