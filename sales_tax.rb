663d398de9bea344396fbd4133bb848aaf9b397d
Michael Atlin <mike.atlin@gmail.com>
2014-01-16 23:53:55 -0500

    Entry 2 error fixed (bad input).  Rounding still off.

--- sales_tax.rb
+++ sales_tax.rb
@@ -36,7 +36,7 @@
  
 # Output 2:
 # 1 imported box of chocolates: 10.50
-# 1 imported bottle of perfume: 54.65
+# 1 imported bottle of perfume: 54.65irb
 # Sales Taxes: 7.65
 # Total: 65.15
  
@@ -138,7 +138,8 @@ receipt1 = Receipt.new(["1 book at 12.49",
 "1 chocolate bar at 0.85"])
 
 
-receipt2 = Receipt.new(["1 imported box of chocolates: 10.50", "1 imported bottle of perfume: 54.65"])
+receipt2 = Receipt.new(["1 imported box of chocolates at 10.00",
+"1 imported bottle of perfume at 47.50"])
 
 receipt3 = Receipt.new(["1 imported bottle of perfume at 27.99","1 bottle of perfume at 18.99","1 packet of headache pills at 9.75","1 box of imported chocolates at 11.25"])
 
